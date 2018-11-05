92f731f6324b032dbb954a07eea019bb94b0b714
taruchu <tmccullough.softwaredeveloper@gmail.com>
2018-11-05 13:04:05 -0800

    Progress for FileManager using MongoDB

---
+++ Erica.MessageBus/FileManager/Interfaces/NoSql/IEricaChatsFilesRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileManager.Interfaces.NoSql
+{
+    public interface IEricaChatsFilesRepository
+    {
+        Task<bool> UploadFileFromBytesAsync(string fileNameGUID, byte[] fileBytes);
+
+        Task<byte[]> DownloadFileAsBytesAsync(string fileNameGUID);
+    }
+}
---
+++ Erica.MessageBus/FileManager/Services/NoSql/EricaChatsFilesDBContext.cs
@@ -0,0 +1,57 @@
+﻿using FileManager.Interfaces.NoSql;
+using FileManager.Models.Configuration;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileManager.Services.NoSql
+{
+    public class EricaChatsFilesDBContext : IEricaChatsFilesRepository
+    {
+        private readonly IMongoDatabase _database = null;
+        private readonly IGridFSBucket _gridFSBucket = null;
+
+        public EricaChatsFilesDBContext(IOptions<Settings> settings)
+        {
+            //TODO Add logging
+
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client == null)
+                throw new ApplicationException("Could not connect to client.");
+            else
+                _database = client.GetDatabase(settings.Value.Database); 
+                
+
+            if (_database == null)
+                throw new ApplicationException("Could not find the collection.");
+            else
+            {
+                _gridFSBucket = new GridFSBucket(_database, new GridFSBucketOptions
+                {
+                    BucketName = "EricaChatsFilesGridFSBucket",
+                    ChunkSizeBytes = 1048576, // 1 MB,
+                    WriteConcern = WriteConcern.WMajority,
+                    ReadPreference = ReadPreference.Secondary
+                });
+            }
+        }
+
+        public async Task<byte[]> DownloadFileAsBytesAsync(string fileNameGUID)
+        {
+            byte[] bytes = await _gridFSBucket.DownloadAsBytesByNameAsync(fileNameGUID);
+            return bytes; 
+        }
+
+        public async Task<bool> UploadFileFromBytesAsync(string fileNameGUID, byte[] fileBytes)
+        {
+            //TODO: Add Metadata
+            ObjectId Id = await _gridFSBucket.UploadFromBytesAsync(fileNameGUID, fileBytes);
+            return (Id != null);
+        }
+    }
+}
